fcf922f1fb7b60d42c50c6899ed38bd6cbb23068
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-09-12 11:22:59 -0300

    Se trabaja las paginas de landlords y reservations

---
+++ frontend/app/components/ContactButton.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const contactButton = () => {
+    return (
+        <>
+            <div
+                className="mt-6 py-3 px-10 cursor-pointer bg-airbnb text-white rounded-xl hover:bg-airbnb-dark transition"
+
+            >
+                Contact
+            </div>
+        </>
+    )
+}
+
+export default contactButton
