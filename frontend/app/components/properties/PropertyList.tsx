61a28ce896b3e97e490b5faa2b112749165916b0
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-09-20 18:54:01 -0300

    Se lista las propiedades usando frontend

--- frontend/app/components/properties/PropertyList.tsx
+++ frontend/app/components/properties/PropertyList.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+'use client'
+import { useState, useEffect } from 'react'
 import PropertyListItem from './PropertyListItem'
 
 const PropertyList = () => {
+
+    const getProperties = async () => {
+        const url = 'http://localhost:8000/api/properties/'
+
+
+        await fetch(url, {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then((json) => {
+                console.log('json', json)
+            })
+            .catch((error) => {
+                console.log('error', error)
+            })
+    };
+
+    useEffect(() => {
+        getProperties()
+    }, [])
+
     return (
         <>
             <PropertyListItem />
